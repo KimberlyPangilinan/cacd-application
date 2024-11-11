67f72ac3e2772c0319fad910cfcbdfdf999cf49a
KimberlyPangilinan <kimberlypangilinan2001@gmail.com>
2024-11-11 23:11:56 +0800

    fix (contact): add type to e

--- src/pages/user/ContactPage.tsx
+++ src/pages/user/ContactPage.tsx
@@ -10,7 +10,7 @@ export const ContactPage = () => {
     message: ''
   });
   
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
